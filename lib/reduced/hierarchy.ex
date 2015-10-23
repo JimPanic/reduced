2f4b20eca1e987db80eebd4a2c7374f9b1b2ec1a
Alexander Pánek <alexander.panek@rise-world.com>
2015-10-23 21:50:57 +0200

    Remove unused code; loading one hierarchy level is enough

--- lib/reduced/hierarchy.ex
+++ lib/reduced/hierarchy.ex
@@ -2,25 +2,6 @@ defmodule Reduced do
   defmodule Hierarchy do
     @interpolation_regex ~r/^(?<datadir>.*)?%{(::)?(?<interpolation_key>[a-zA-Z_-]+)}$/u
 
-    def load(datadir, [level]) do
-      load(datadir, level)
-    end
-
-    def load(datadir, [level|levels]) do
-      List.flatten([
-        load(datadir, level),
-        load(datadir, levels)
-      ])
-    end
-
-    #Path.split(level)
-    #|> Enum.map_reduce("", fn (level_part, acc) ->
-    #{
-    #  Path.join([datadir, acc, level_part]),
-    #  Path.join([acc, level_part])
-    #}
-    #end)
-
     def load(datadir, level) do
       [first|rest] = Path.split(level)
       first_path   = Path.join([datadir, first])
@@ -34,7 +15,7 @@ defmodule Reduced do
               true  -> 
                 case File.regular?(Path.join([relative_datadir, option])) do
                   true  -> {interpolation_key, option}
-                  false -> {interpolation_key, {option, options_for(first_path)}}
+                  false -> {interpolation_key, option, options_for(first_path)}
                 end
 
               false ->
@@ -53,13 +34,6 @@ defmodule Reduced do
           end
       end
     end
-
-    #def options_for(interpolation_key, [head|tail]) do
-    #  List.flatten([
-    #    options_for(interpolation_key, head),
-    #    options_for(interpolation_key, tail)
-    #  ])
-    #end
 
     def options_for(path) do
       case File.exists?(path) do
