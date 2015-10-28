abe3475788c6916d7a69d7c1781842cba770deb3
Alexander Pánek <alexander.panek@rise-world.com>
2015-10-28 13:25:10 +0100

    Split up conditional code into several functions

--- lib/reduced/hierarchy.ex
+++ lib/reduced/hierarchy.ex
@@ -1,45 +1,56 @@
-defmodule Reduced do
-  defmodule Hierarchy do
-    @interpolation_regex ~r/^(?<datadir>.*)?%{(::)?(?<interpolation_key>[a-zA-Z_-]+)}$/u
+require IEx
 
-    def load(datadir, level) do
-      [first|rest] = Path.split(level)
-      first_path   = Path.join([datadir, first])
+defmodule Reduced.Hierarchy do
+  @interpolation_regex ~r/^(?<datadir>.*)?%{(::)?(?<interpolation_key>[a-zA-Z_-]+)}$/u
 
-      case Regex.named_captures(@interpolation_regex, first_path) do
-        %{"interpolation_key" => interpolation_key,
-          "datadir"           => relative_datadir} ->
+  def load(datadir, level) do
+    [first|rest] = Path.split(level)
+    first_path   = Path.join([datadir, first])
 
-          Enum.map(options_for(relative_datadir), fn (option) ->
-            case Enum.empty?(rest) do
-              true  -> 
-                case File.regular?(Path.join([relative_datadir, option])) do
-                  true  -> {interpolation_key, option}
-                  false -> {interpolation_key, option, options_for(first_path)}
-                end
+    interpolate_or_recurse(first_path, rest, Regex.named_captures(@interpolation_regex, first_path))
+  end
 
-              false ->
-                {
-                  interpolation_key,
-                  option,
-                  load(Path.join([relative_datadir, option]), Path.join(rest))
-                }
-            end
-          end)
+  def interpolate_or_recurse(first_path, rest, nil) do
+    load(first_path, Path.join(rest))
+  end
 
-        nil ->
-          case Enum.empty?(rest) do
-            true  -> options_for(first_path)
-            false -> load(first_path, Path.join(rest)) 
-          end
-      end
+  def interpolate_or_recurse(first_path, [], nil) do
+    options_for(first_path)
+  end
+
+  def interpolate_or_recurse(first_path, rest, %{"interpolation_key" => interpolation_key, "datadir" => relative_datadir}) do
+    Enum.map(options_for(relative_datadir), &(interpolate(first_path, Path.join([relative_datadir, &1]), interpolation_key, &1, rest)))
+  end
+
+  def interpolate(first_path, option_path, interpolation_key, option, []) do
+    interpolated_tuple(first_path, interpolation_key, option, {:is_regular_file, File.regular?(option_path)})
+  end
+
+  def interpolate(_, option_path, interpolation_key, option, level_parts) do
+    {interpolation_key, option, load(option_path, Path.join(level_parts))}
+  end
+
+  def interpolated_tuple(_, interpolation_key, option, {:is_regular_file, true}) do
+    {interpolation_key, option}
+  end
+
+  def interpolated_tuple(path, interpolation_key, option, {:is_regular_file, false}) do
+    {interpolation_key, option, options_for(path)}
+  end
+
+  def options_for(path) do
+    options_for(path, {:file_exists, File.exists?(path)})
+  end
+
+  def options_for(path, {:file_exists, true}) do
+    case File.ls(path) do
+      {:ok, files}   ->
+        files
+      {:error, reason} -> [] # TODO: Log the reason
     end
+  end
 
-    def options_for(path) do
-      case File.exists?(path) do
-        true  -> File.ls!(path)
-        false -> []
-      end
-    end
+  def options_for(path, {:file_exists, false}) do
+    []
   end
 end
