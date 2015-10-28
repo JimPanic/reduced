f3ac6135309b356b30d526f69ce73ad94e11f420
Alexander Pánek <alexander.panek@rise-world.com>
2015-10-28 13:25:31 +0100

    Flatten all keys
    That means: duplicating the whole hierarchy branch for each key/value
    pair.

--- lib/reduced/flatten.ex
+++ lib/reduced/flatten.ex
@@ -1,42 +1,40 @@
-defmodule Reduced do
-  defmodule Flatten do
-    def nested_keys([{key, value}]) do
-      nested_keys({key, value})
-    end
+defmodule Reduced.Flatten do
+  def nested_keys([{key, value}]) do
+    nested_keys({key, value})
+  end
 
-    def nested_keys([{key, value}|tail]) do
-      List.flatten([
-        nested_keys({key, value}),
-        nested_keys(tail)
-      ])
-    end
+  def nested_keys([{key, value}|tail]) do
+    List.flatten([
+      nested_keys({key, value}),
+      nested_keys(tail)
+    ])
+  end
 
-    def nested_keys({key, [{inner_key, value}]}) do
-      nested_keys({"#{key}::#{inner_key}", value})
-    end
+  def nested_keys({key, [{inner_key, value}]}) do
+    nested_keys({"#{key}::#{inner_key}", value})
+  end
 
-    def nested_keys({key, [{inner_key, value}|tail]}) do
-      List.flatten([
-        nested_keys({"#{key}::#{inner_key}", value}),
-        nested_keys({key, tail})
-      ])
-    end
+  def nested_keys({key, [{inner_key, value}|tail]}) do
+    List.flatten([
+      nested_keys({"#{key}::#{inner_key}", value}),
+      nested_keys({key, tail})
+    ])
+  end
 
-    def nested_keys({key, value}) when not is_map(value) do
-      case value do
-        {inner_key, inner_value} ->
-          nested_keys({"#{key}::#{inner_key}", inner_value})
-        _ ->
-          {to_string(key), to_string(value)}
-      end
+  def nested_keys({key, value}) when not is_map(value) do
+    case value do
+      {inner_key, inner_value} ->
+        nested_keys({"#{key}::#{inner_key}", inner_value})
+      _ ->
+        {to_string(key), to_string(value)}
     end
+  end
 
-    def nested_keys({key, value}) when is_map(value) do
-      nested_keys({key, nested_keys(value)})
-    end
+  def nested_keys({key, value}) when is_map(value) do
+    nested_keys({key, nested_keys(value)})
+  end
 
-    def nested_keys(data) when is_map(data) do
-      Map.to_list(data)
-    end
+  def nested_keys(data) when is_map(data) do
+    Map.to_list(data)
   end
 end
